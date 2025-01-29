e2670110d2e6b0e804e4fa21395cecda4c7ee463
Dakin Quelia <dakinquelia@gmail.com>
2025-01-29 10:01:52 +0100

    Ajout de la classe "Game"

--- src/core/game.ts
+++ src/core/game.ts
@@ -1,5 +1,22 @@
 /*****************************************
-*	GameTextTS        :: Game
-*	Auteur 		       : Dakin Quelia
-*	Version 	       : 1.0.0. 
+*	Gestion du jeu
+*   --------------
+*	Auteur 		: Dakin Quelia
+*	Version 	: 1.0.0. 
 *****************************************/
+import Config from "../core/config/global.ts";
+
+class Game 
+{
+    /**
+    *   Le constructeur
+    * 
+    *   @return {void}
+    **/
+    constructor()
+    {
+        
+    }
+}
+
+export default new Game();
