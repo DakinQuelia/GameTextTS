e6c19a54fc855958cff77b9be66efe0b85db940d
Dakin Quelia <dakinquelia@gmail.com>
2025-02-01 07:44:30 +0100

    Ajout du type "Classe"

---
+++ src/core/types/classe.ts
@@ -0,0 +1,11 @@
+/*****************************************
+*	GameTextTS :: types/class.ts
+*   ----------------------------
+*	Auteur 		: Dakin Quelia
+*	Version 	: 1.0.0. 
+*****************************************/
+export type Class = { 
+    name: string,
+    color: string,
+    level: number
+};
